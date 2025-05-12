f8e91c3991058df8fb650560f81f164a55ce57e5
Chuck <tnqkr3494@naver.com>
2025-05-12 12:11:57 +0900

    [level 2] Title: 올바른 괄호, Time: 29.76 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,20 @@
+function solution(s){
+    const stack = [];
+        
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === "("){
+            stack.push(s[i])
+        }
+        else{
+            if(stack.length > 0){
+                stack.pop()
+            }
+            else{
+                return false
+            }
+        }
+    }
+    
+    return stack.length === 0
+    
+}
