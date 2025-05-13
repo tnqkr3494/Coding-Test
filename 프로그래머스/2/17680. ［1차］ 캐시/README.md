7446e16506f6fe216b050dd43465142598d17c90
Chuck <tnqkr3494@naver.com>
2025-05-13 14:21:01 +0900

    [level 2] Title: [1차] 캐시, Time: 44.41 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680#) 
 
 ### 성능 요약
 
-메모리: 42.8 MB, 시간: 31.44 ms
+메모리: 41.6 MB, 시간: 44.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 13일 14:15:00
+2025년 05월 13일 14:20:59
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,26 +1,33 @@
+let answer = 0;
+
+function check(arr, name){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === name){
+            arr.splice(i, 1);
+            answer++;
+            return;
+        }
+    }
+    answer += 5
+    return;
+}
+
+
 function solution(cacheSize, cities) {
-    let answer = 0;
     const cache = [];
     
-    if (cacheSize === 0) {
-        return cities.length * 5;
-    }
-    
-    for (let city of cities) {
+    for(let city of cities){
         city = city.toLowerCase();
-        const index = cache.indexOf(city);
-        
-        if (index !== -1) { // 캐시 히트
-            cache.splice(index, 1); // 기존 위치에서 제거
-            answer += 1;
-        } else { // 캐시 미스
-            answer += 5;
-            if (cache.length >= cacheSize) {
-                cache.shift(); // 가장 오래된 항목 제거
-            }
+        check(cache, city);
+        if(cache.length >= cacheSize){
+            cache.shift();
         }
-        cache.push(city); // 최신 항목으로 추가
+        
+        if(cache.length < cacheSize){
+           cache.push(city);    
+        }
+        
     }
     
-    return answer;
+    return answer
 }
