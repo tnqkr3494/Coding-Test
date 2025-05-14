4e7e1906d8e6b915eb88c8f70c9e4a6178a1c476
Chuck <tnqkr3494@naver.com>
2025-05-14 15:17:14 +0900

    [level 2] Title: 모음 사전, Time: 6.17 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -3,19 +3,30 @@ function solution(word) {
  const result = []
  
  function sol(depth, ch){
+     let flag = false;
+     
      if(depth > 5){
          return;
      }
      else{
          // 여기서 몇번째인지 return
          const temp = ch.join("");
+         if(temp === word){
+             result.push(temp);
+             flag = true;
+             return;
+         }
          result.push(temp);
+
      }
      
      for(let i = 0; i < 5; i++){
          ch.push(vowel[i]);
          sol(depth + 1, ch)
          ch.pop();
+         if(flag){
+             return
+         }
      }
  }
      sol(0, []);
