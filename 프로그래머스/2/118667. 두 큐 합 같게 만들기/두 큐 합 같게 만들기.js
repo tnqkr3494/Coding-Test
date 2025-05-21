6e50f9defb4deb1dc288f09ec2c3c87a56f4ddd9
Chuck <tnqkr3494@naver.com>
2025-05-21 13:21:19 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 65.26 ms, Memory: 66.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -0,0 +1,46 @@
+class Queue{
+    items = [];
+    start = 0;
+    end = 0;
+    
+    push(value){
+        this.items.push(value);
+        this.end++;
+    }
+    pop(){
+        return this.items[this.start++];
+    }
+}
+
+function solution(queue1, queue2) {
+    let answer = 0;
+    const q1 = new Queue();
+    const q2 = new Queue();
+    let [sumQ1, sumQ2] = [queue1.reduce((acc, cur) => acc + cur), queue2.reduce((acc, cur) => acc + cur)];
+    
+    for(let i = 0; i < queue1.length; i++){
+        q1.push(queue1[i])
+        q2.push(queue2[i])
+    }
+    
+    for(let i = 0; i <= 300000; i++){
+        if(sumQ1 === sumQ2){
+            break;
+        }
+        else if(sumQ1 < sumQ2){
+            const num = q2.pop();
+            sumQ2 -= num;
+            sumQ1 += num;
+            q1.push(num);
+        }
+        else{
+            const num = q1.pop();
+            sumQ1 -= num;
+            sumQ2 += num;
+            q2.push(num);
+        }
+        answer++;
+    }
+    
+    return sumQ1 === sumQ2 ? answer : -1;
+}
